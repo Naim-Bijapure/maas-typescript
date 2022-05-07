366a14cd53bec73b6fd82d73d39a3f95bcae773d
NaimBijapure <naim@vinnovatetechnologies.com>
2022-05-07 20:39:50 +0530

    Title: update( ui changes)

--- packages/hardhat-ts/contracts/MultiSigFactory.sol
+++ packages/hardhat-ts/contracts/MultiSigFactory.sol
@@ -7,9 +7,26 @@ contract MultiSigFactory {
   MultiSigWallet[] public multiSigs;
   mapping(address => bool) existsMultiSig;
 
+<<<<<<< Updated upstream
   event Create(uint256 indexed contractId, address indexed contractAddress, address creator, address[] owners, uint256 signaturesRequired);
 
   event Owners(address indexed contractAddress, address[] owners, uint256 indexed signaturesRequired);
+=======
+  event Create(
+    uint indexed contractId,
+    address indexed contractAddress,
+    address creator,
+    address[] owners,
+    uint signaturesRequired
+  );
+
+  event Owners(
+    address indexed contractAddress,
+    address[] owners,
+    uint256 indexed signaturesRequired
+  );
+
+>>>>>>> Stashed changes
 
   constructor() {}
 
@@ -29,11 +46,19 @@ contract MultiSigFactory {
   function create(
     uint256 _chainId,
     address[] memory _owners,
+<<<<<<< Updated upstream
     uint256 _signaturesRequired
   ) public payable {
     uint256 id = numberOfMultiSigs();
 
     MultiSigWallet multiSig = (new MultiSigWallet){ value: msg.value }(_chainId, _owners, _signaturesRequired, address(this));
+=======
+    uint _signaturesRequired
+  ) public payable {
+    uint id = numberOfMultiSigs();
+
+    MultiSigWallet multiSig = (new MultiSigWallet){value: msg.value}(_chainId, _owners, _signaturesRequired, address(this));
+>>>>>>> Stashed changes
     multiSigs.push(multiSig);
     existsMultiSig[address(multiSig)] = true;
 
@@ -41,7 +66,11 @@ contract MultiSigFactory {
     emit Owners(address(multiSig), _owners, _signaturesRequired);
   }
 
+<<<<<<< Updated upstream
   function numberOfMultiSigs() public view returns (uint256) {
+=======
+  function numberOfMultiSigs() public view returns(uint) {
+>>>>>>> Stashed changes
     return multiSigs.length;
   }
 
@@ -50,6 +79,7 @@ contract MultiSigFactory {
     view
     returns (
       address multiSigAddress,
+<<<<<<< Updated upstream
       uint256 signaturesRequired,
       uint256 balance
     )
@@ -58,3 +88,12 @@ contract MultiSigFactory {
     return (address(multiSig), multiSig.signaturesRequired(), address(multiSig).balance);
   }
 }
+=======
+      uint signaturesRequired,
+      uint balance
+    ) {
+      MultiSigWallet multiSig = multiSigs[_index];
+      return (address(multiSig), multiSig.signaturesRequired(), address(multiSig).balance);
+    }
+}
+>>>>>>> Stashed changes
