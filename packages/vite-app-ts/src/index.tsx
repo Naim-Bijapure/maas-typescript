a953a48b0c9cc645c516f6923f7812c372c89867
NaimBijapure <naim@vinnovatetechnologies.com>
2022-05-16 12:01:14 +0530

    Title: update(react 18 adjustment on index.tx file)

--- packages/vite-app-ts/src/index.tsx
+++ packages/vite-app-ts/src/index.tsx
@@ -14,21 +14,26 @@
  * Loads {@see App} which sets up the application async.
  * The main page is in the component {@see MainPage}
  */
+// import ReactDOM from 'react-dom/client';
+
 const run = async (): Promise<void> => {
   await import('./helpers/__global');
   // dynamic imports for code splitting
   const { lazy, Suspense, StrictMode } = await import('react');
-  const ReactDOM = await import('react-dom');
+  // const ReactDOM = await import('react-dom');
+  const ReactDOM = await import('react-dom/client');
 
   const App = lazy(() => import('./App'));
 
-  ReactDOM.render(
+  //@ts-ignore
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  root.render(
     <StrictMode>
       <Suspense fallback={<div />}>
         <App />
       </Suspense>
-    </StrictMode>,
-    document.getElementById('root')
+    </StrictMode>
   );
   // const root = ReactDOM.createRoot(document.getElementById('root'));
   // root.render(
